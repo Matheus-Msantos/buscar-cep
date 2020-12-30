d61852fe0eeb66a004597d243c177c71dfea7a78
Matheus-Msantos <matheusmelosantos10@gmmail.com>
2020-12-30 18:37:57 -0300

    Preenchendo input automáticamente

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-  const cep = document.querySelector('#cep');
+  const cep = document.querySelector('#code');
+
+  const showData = (result) => {
+    for (const camp in result) {
+      if (document.querySelector('#' + camp)) {
+        document.querySelector('#' + camp).value = result[camp];
+      }
+    }
+  }
+
 
   cep.addEventListener("blur", (e) => {
     console.log(cep.value);
@@ -15,7 +24,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     fetch(`https://ws.apicep.com/cep/${search}.json`, options)
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => showData(data))
       .catch(e => console.log('Erro: ' + e, e.message))
   })
 
