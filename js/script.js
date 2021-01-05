8b9a82cb7bbc9152f3a84e6cc20cd8c40fef881e
Matheus-Msantos <matheusmelosantos10@gmail.com>
2021-01-05 18:47:37 -0300

    Excluindo FOR

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 function redirectPage(event) {
+
   event.preventDefault();
+
 }
 
 function validate() {
+
   document.querySelector('#info').textContent = '';
   document.querySelector('#messageErro').textContent = '';
   document.getElementById('code').style.borderColor = "grey";
@@ -10,45 +13,45 @@ function validate() {
   const validateCep = document.querySelector('#code');
 
   if (validateCep.value == '') {
+
     document.querySelector('#messageErro').textContent = 'Cep inválido';
     document.getElementById('code').style.borderColor = "#FF9C9C";
+
   }
 
 }
-
-
 
 window.addEventListener('DOMContentLoaded', function () {
   const cep = document.querySelector('#code');
 
   const showData = (result) => {
-    for (const camp in result) {
 
-      if (result['ok'] !== true) {
+    if (result['ok'] !== true) {
 
-        document.getElementById('textHidden').style.borderColor = "#FF9C9C";
-        document.querySelector('#message').textContent = 'Resultado não encontrado';
-        document.querySelector('#address').textContent = '';
-        document.querySelector('#district').textContent = '';
-        document.querySelector('#city').textContent = '';
-        document.querySelector('#state').textContent = '';
+      document.getElementById('textHidden').style.borderColor = "#FF9C9C";
+      document.querySelector('#message').textContent = 'Resultado não encontrado';
+      document.querySelector('#address').textContent = '';
+      document.querySelector('#district').textContent = '';
+      document.querySelector('#city').textContent = '';
+      document.querySelector('#state').textContent = '';
 
-      } else if (document.querySelector('#' + camp)) {
+    } else {
 
-        document.getElementById('textHidden').style.borderColor = "#9CC4FF";
-        document.querySelector('#messageErro').textContent = '';
-        document.querySelector('#address').textContent = result['address'] + ',';
-        document.querySelector('#district').textContent = result['district'] + ',';
-        document.querySelector('#city').textContent = result['city'] + '-';
-        document.querySelector('#state').textContent = result['state'];
-        document.querySelector('#message').textContent = '';
+      document.getElementById('textHidden').style.borderColor = "#9CC4FF";
+      document.querySelector('#messageErro').textContent = '';
+      document.querySelector('#address').textContent = result['address'] + ',';
+      document.querySelector('#district').textContent = result['district'] + ',';
+      document.querySelector('#city').textContent = result['city'] + '-';
+      document.querySelector('#state').textContent = result['state'];
+      document.querySelector('#message').textContent = '';
 
-      }
     }
+
   }
 
 
   cep.addEventListener("blur", (e) => {
+
     console.log(cep.value);
 
     let search = cep.value.replace('-', '');
@@ -64,7 +67,5 @@ window.addEventListener('DOMContentLoaded', function () {
       .then(data => showData(data))
       .catch(e => console.log('Erro: ' + e, e.message))
   })
-
-
 
 });
